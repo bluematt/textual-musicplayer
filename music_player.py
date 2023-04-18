0e1a04d121986f95bda59898b3d4be65d75724d1
bluematt <307723+bluematt@users.noreply.github.com>
2023-04-18 14:23:44 +0100

    Playing tracks from the playlist

--- music_player.py
+++ music_player.py
@@ -4,15 +4,24 @@ import sys
 from os import walk, path
 from typing import ClassVar
 
-from textual.reactive import reactive
 from tinytag import TinyTag
 import pygame
+
+from textual import log
+from textual.reactive import reactive
 from textual.app import App, ComposeResult, CSSPathType
 from textual.containers import Container, Horizontal, Center, Vertical, VerticalScroll
 from textual.widgets import Header, Footer, Static, Button, Switch, Label, DataTable, Placeholder
 from tinytag.tinytag import ID3, Ogg, Wave, Flac, Wma, MP4, Aiff
 
-# The support file types. @TODO
+TrackType = TinyTag | ID3 | Ogg | Wave | Flac | Wma | MP4 | Aiff
+Track = tuple[str, ...]
+
+# Index of the file column of the Track tuple.
+TRACK_FILE_OFFSET: int = 5
+
+# The supported file types.
+# TODO Determine while audio file types are/can be supported.
 TRACK_EXT: tuple[str, ...] = (".mp3",
                               # ".mp4",
                               # ".m4a",
@@ -82,7 +91,7 @@ class MusicPlayerApp(App):
     BINDINGS = [
         ("space", "toggle_play", "Play/Pause"),
         ("backspace", "stop_play", "Stop"),
-        ("t", "next_track", "Next track"),
+        # ("t", "next_track", "Next track"),
         ("m", "toggle_mute", "Mute/Unmute"),
         ("d", "toggle_dark", "Toggle dark mode"),
         ("o", "open_directory", "Open directory"),
@@ -91,17 +100,37 @@ class MusicPlayerApp(App):
         ("q", "quit", "Quit"),
     ]
 
-    # Whether the app is currently playing.
-    playing: reactive[bool] = reactive(False)
     # Whether the app is muted.
     mute: reactive[bool] = reactive(False)
 
     # The current working directory (where music files are).
     cwd: reactive[str] = reactive('./demo_music')
+
     # The list of current tracks.
     tracks: reactive[list[tuple]] = reactive([])
 
-    current_track: int = 1
+    # The current track.
+    current_track: reactive[Track | None] = reactive(None)
+
+    # def watch_cwd(self) -> None:
+    #     """Watch for changes to `cwd`."""
+    #     log("CWD CHANGED")
+    #     self.scan_track_directory()
+
+    # def watch_tracks(self) -> None:
+    #     """Watch for changes to `tracks`."""
+    #     log("PLAYLIST UPDATED")
+    #     self.update_track_list()
+
+    def watch_current_track(self) -> None:
+        """Watch for changes to `current_track`."""
+        log("CURRENT_TRACK_CHANGED")
+        if self.is_playing():
+            self.play_current_track()
+
+    def play_current_track(self) -> None:
+        """Play the current track."""
+        self.play_track(self.current_track)
 
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
@@ -111,12 +140,15 @@ class MusicPlayerApp(App):
     def on_mount(self) -> None:
         # Scan for music in the current working directory.
         self.scan_track_directory()
-        self.update_track_list()
+        self.update_playlist()
 
-        self.set_focus(self.query_one("#playlist"))
+        playlist = self.query_one("#playlist", DataTable)
+        self.set_focus(playlist)
+        self.current_track = playlist.get_row_at(0)
 
-    def update_track_list(self) -> None:
-        playlist: DataTable = self.query_one("#playlist")
+    def update_playlist(self) -> None:
+        """Get the list of """
+        playlist: DataTable = self.query_one("#playlist", DataTable)
         tracks = iter(self.tracks)
         playlist.add_columns(*next(tracks))
         playlist.add_rows(tracks)
@@ -126,10 +158,15 @@ class MusicPlayerApp(App):
         self.dark = not self.dark
 
     def action_toggle_play(self) -> None:
-        self.playing = not self.playing
-        self.sub_title = "|>" if self.playing else "||"
-        if self.playing:
-            self.play_track(self.current_track)
+        pygame.mixer.init()
+        if self.is_playing():
+            self.pause()
+        else:
+            self.play_current_track()
+
+    def pause(self):
+        pygame.mixer.init()
+        pygame.mixer.music.pause()
 
     def action_toggle_mute(self) -> None:
         self.mute = not self.mute
@@ -148,53 +185,62 @@ class MusicPlayerApp(App):
     def action_open_directory(self) -> None:
         pass
 
-    def action_next_track(self) -> None:
-        self.current_track += 1
-        self.log(f"Track: {self.current_track}")
-        if self.current_track >= len(self.tracks):
-            self.current_track = 1
-        self.play_track(self.current_track)
+    # def action_next_track(self) -> None:
+    #     self.current_track += 1
+    #     self.log(f"Track: {self.current_track}")
+    #     if self.current_track >= len(self.tracks):
+    #         self.current_track = 1
+    #     # self.play_track(self.current_track)
 
     def action_scan_track_directory(self) -> None:
         self.scan_track_directory()
 
     def scan_track_directory(self) -> None:
+        """Scan the current working directory for music files."""
         files: list[bytes | str] = [path.join(dir_path, f)
                                     for (dir_path, _dir_names, filenames) in walk(self.cwd)
                                     for f in filenames if
                                     f.endswith(TRACK_EXT) and not f.startswith(".")]
-        tracks: list[TinyTag | ID3 | Ogg | Wave | Flac | Wma | MP4 | Aiff] = [TinyTag.get(f)
-                                                                              for f in files]
 
-        track_data: list[tuple[int | str, str, str, str, str, str, str]] = [
-            ("Track", "Title", "Artist", "Album", "Length", "Genre", "File"), ]
-        [track_data.append((idx + 1, t.title, t.artist, t.album, t.duration, t.genre, files[idx])) for idx, t in
+        # Get track metadata from music files.
+        tracks: list[TrackType] = [TinyTag.get(f) for f in files]
+
+        # Create a list of tuple(track info).
+        track_data: list[Track] = [("Title", "Artist", "Album", "Length", "Genre", "File"), ]
+        [track_data.append((t.title, t.artist, t.album, t.duration, t.genre, files[idx])) for idx, t in
          enumerate(tracks)]
+
         self.tracks = track_data
 
-    def play_track(self, track_id: int) -> None:
-        self.log(self.tracks[track_id])
-        self.stop_track()
+    def update_track_info(self, track: Track) -> None:
+        log(track)
 
-        track = self.tracks[track_id]
-        file = track[6]
-        pygame.mixer.music.load(file)
-        pygame.mixer.music.play()
-
-        self.update_track_info(track)
-
-    def update_track_info(self, track: tuple) -> None:
-        pass
-
-    def stop_track(self) -> None:
+    def stop_music(self) -> None:
         pygame.mixer.init()
+        pygame.mixer.music.stop()
+        pygame.mixer.music.unload()
 
-        if pygame.mixer.music.get_busy():
-            pygame.mixer.music.stop()
-            pygame.mixer.music.unload()
+    def is_playing(self) -> bool:
+        pygame.mixer.init()
+        return pygame.mixer.music.get_busy()
 
-        self.playing = False
-        self.sub_title = "[]"
+    def play_track(self, track: Track):
+        """Play a track."""
+        if track:
+            if self.is_playing():
+                self.stop_music()
+
+            file = track[TRACK_FILE_OFFSET]
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(file)
+            pygame.mixer.music.play()
+
+            self.update_track_info(track)
+
+    def on_data_table_row_selected(self, event: DataTable.RowSelected) -> None:
+        """Handler for selecting a row in the data table."""
+        self.current_track = tuple(event.data_table.get_row_at(event.cursor_row))
 
 
 if __name__ == "__main__":
